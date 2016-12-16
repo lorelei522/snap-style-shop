969d1f54eec9b1860bdace500564c84cd34e3f04
Lorelei Lopez <email": null>
2016-12-16 12:48:08 -0500

    Add ability to view products searched for

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,22 @@ class ProductsController < ApplicationController
   #does it get passed like "black dress" or "black+dress" to shopstyle
   #because of how the json string comes back use ["products"] to get the actual products
   def create
-    @products = []
+  end
+
+  def index
     query_params = params[:query]
     limit = 4
     shopstyle_response_api = open("http://api.shopstyle.com/api/v2/products?pid=#{Dotenv.load["SHOPSTYLE_TOKEN"]}&fts=#{query_params}&offset=0&limit=#{limit}").read
     shopstyle_response = JSON.parse(shopstyle_response_api)["products"]
-    pp @products = shopstyle_response
-    # redirect_to product_path(@products)
+    @products = shopstyle_response.map! do |product|
+      {
+        image: product['image']['sizes']['XLarge']['url'],
+        link: product['clickUrl'],
+        name: product['name'],
+        price: product['priceLabel'],
+      }
+    end
+    @products
     #shopstyle_products = Product.create() USE THIS IF WE WANT TO SAVE EACH RETURN INTO THE database
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :products
   root :to => 'welcome#index'
 
+
+  get '/results' => 'products#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
