a5b5c32d22dd6530f88935368a282dc282531619
Lorelei Lopez <email": null>
2016-12-16 18:35:49 -0500

    user profile - fix profile photo rendering

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  attr_accessor :image_file_name
   has_attached_file :image , styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :products
   resources :users
 
+  get 'sessions/new' => 'sessions#new'
+  post 'sessions/login_attempt' => 'sessions#login_attempt'
+  delete 'sessions' => 'sessions#destroy'
+
+  resources :sessions, except: :destroy
+
   get '/results' => 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
