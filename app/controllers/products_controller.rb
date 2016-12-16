4d43afaefe50964666c1942d5e8e51b7921020a4
Lorelei Lopez <email": null>
2016-12-16 10:32:14 -0500

    product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,7 @@ class ProductsController < ApplicationController
     limit = 4
     shopstyle_response_api = open("http://api.shopstyle.com/api/v2/products?pid=#{Dotenv.load["SHOPSTYLE_TOKEN"]}&fts=#{query_params}&offset=0&limit=#{limit}").read
     shopstyle_response = JSON.parse(shopstyle_response_api)["products"]
-    @products = shopstyle_response
+    pp @products = shopstyle_response
     # redirect_to product_path(@products)
     #shopstyle_products = Product.create() USE THIS IF WE WANT TO SAVE EACH RETURN INTO THE database
   end
@@ -19,7 +19,4 @@ private
       "http://api.shopstyle.com/api/v2/products?pid="
     end
 
-    def shopstyle_token2
-      "uid2849-2518543-97"
-    end
 end
