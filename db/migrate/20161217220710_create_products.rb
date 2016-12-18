093752ac76696c867ffb3b4452f5c9e24b7ade8a
Lorelei Lopez <email": null>
2016-12-18 17:36:17 -0500

    in the middle of migrations

--- db/migrate/20161217220710_create_products.rb
+++ db/migrate/20161217220710_create_products.rb
@@ -1,6 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.string :brand
+      t.string :size
+      t.string :
 
       t.timestamps
     end
