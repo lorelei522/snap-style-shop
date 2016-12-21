29a1926098bd9b9b36c21e4d15c0cd1ac4f8f22f
“Sofia <sofiaclara93@gmail.com>
2016-12-21 17:30:12 -0500

    restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
-  resources :products
-  resources :users
-  resources :welcome
+  resources :products, only: :index
+  resources :users, except: :destroy
+  resources :welcome, only: :index
   resources :favorites, except: [:update, :show]
-  resources :items
+  resources :items, only: [:new, :create]
 
 
   post 'sessions/login_attempt' => 'sessions#login_attempt'
   delete 'sessions' => 'sessions#destroy'
 
-  resources :sessions, except: :destroy
+  resources :sessions, except: [:destroy, :update, :edit, :index]
 
 end
