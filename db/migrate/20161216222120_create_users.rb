cba5da38a1caef526e39003b4bf2a3e2a7f70c67
“Sofia <sofiaclara93@gmail.com>
2016-12-21 17:11:50 -0500

    add uniqueness on tables

--- db/migrate/20161216222120_create_users.rb
+++ db/migrate/20161216222120_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
+      t.string :username, null: false, unique: true
       t.string :password_digest, null: false
-      t.string :email, null: false
+      t.string :email, null: false, unique: true
       t.string :aboutme
       t.timestamps
     end
