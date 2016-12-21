36280f802a15f77665484f7575e46aae19877182
“Sofia <sofiaclara93@gmail.com>
2016-12-21 17:36:36 -0500

    remove comments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,10 @@
 
 class ProductsController < ApplicationController
-  #making the search_keyword split? when we get the GOOGLE VISION to work
-  #does it get passed like "black dress" or "black+dress" to shopstyle
-  #because of how the json string comes back use ["products"] to get the actual products
-
   def index
     if logged_in?
       @products = shopstylecall
     else
       redirect_to new_session_path
-    end 
+    end
   end
 end
